92278098126f591ff4f35b5fc0f69080f9d53d18
durjoy-10 <ug2102017@cse.pstu.ac.bd>
2025-03-11 20:22:10 +0600

    add django live_server

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'tailwind',
     'theme',
     'django_extensions',
+    'livereload',
 ]
 
 TAILWIND_APP_NAME = 'theme',
@@ -57,6 +58,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'livereload.middleware.LiveReloadScript',
 ]
 
 ROOT_URLCONF = 'ecommerce.urls'
