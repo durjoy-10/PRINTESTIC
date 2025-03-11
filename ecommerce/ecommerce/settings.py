e656a69dc5e593a8a3f1aaec981733501b076fe7
durjoy-10 <ug2102017@cse.pstu.ac.bd>
2025-03-11 23:11:06 +0600

    add requirments.txt

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-30l_6tp510j6k(f$vsui-9kfb!#8mxs=vvkbs4vp_+v*xa5gev'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
